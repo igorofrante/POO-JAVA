c8e9fc85dea44421e02b024113066b9e591ccf72
igoro <igoro@LAPTOP-FSBT12NI>
2020-10-07 20:36:14 -0300

    Primeiras classes

---
+++ contaBancaria.java
@@ -0,0 +1,27 @@
+
+public class contaBancaria extends Cliente {
+	private int agencia;
+	private int conta;
+	private double saldo;
+	
+	public contaBancaria(int cpf, String nome, int agencia, int conta, double saldo) {
+		super(cpf, nome);
+		this.agencia = agencia;
+		this.conta = conta;
+		this.saldo = saldo;
+	}
+	
+	
+	
+
+	
+	
+	
+
+	
+
+	
+	
+
+
+}
