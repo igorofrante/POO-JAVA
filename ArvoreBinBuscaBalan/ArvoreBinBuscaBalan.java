76272ebc743f152dd61e0872cc8d859081f00b86
igoro <igoro@LAPTOP-FSBT12NI>
2020-10-12 20:03:34 -0300

    .....

--- ArvoreBinBuscaBalan/ArvoreBinBuscaBalan.java
+++ ArvoreBinBuscaBalan/ArvoreBinBuscaBalan.java
@@ -1,9 +1,9 @@
 package ArvoreBinBuscaBalan;
 
-import dados.Item;
 
 public class ArvoreBinBuscaBalan {
 	private NoArvore raiz;
+	
 	public ArvoreBinBuscaBalan(){
 		this.raiz = null;
 	}
--- dados/Item.java
+++ dados/Item.java
@@ -4,18 +4,11 @@ public class Item {
 	// aqui podem ser declarados outros atributos conforme sua necessidade.
 	private int chave;
 
-	
-	
 	//Construtor de objetos da classe Item
 	public Item(int ch) {
 		this.chave = ch;
 	}
 	
-
-
-
-
-
 	//Modifica o valor do atributo chave
 	public void setChave (int ch){
 		this.chave = ch;
